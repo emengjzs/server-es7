4d5020e831d54777bff23c31e726c52e1f64dd5d
Jiao <I337228@global.corp.sap>
2016-11-30 15:28:58 +0800

    add server.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-(async () => {
-    console.log("hello world");
-    return "hello, world";
-})();
+import httpService from "./lib/http/server";
+
+const port = 8000;
+httpService.listen(port, () => {
+    console.log(`server is now listening on port ${port}`);
+})
