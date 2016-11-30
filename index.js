4efb177a57edcf33a2c005aa31beb94ee024dc89
Jiao <I337228@global.corp.sap>
2016-11-30 15:45:10 +0800

    add index page

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import httpService from "./lib/http/server";
-import app from "./app";
+import app from "./lib/app";
 
 const port = 8000;
 
-httpService.on("request", app);
+app.attach(httpService);
 
 httpService.listen(port, () => {
     console.log(`server is now listening on port ${port}`);
