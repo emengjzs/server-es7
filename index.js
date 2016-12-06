a165b8cd3047145cea4ddbdad90641dde3d1aac9
Jiao <I337228@global.corp.sap>
2016-12-06 11:08:01 +0800

    Integretec config with index.js and do some refactor.

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
+import config from "config";
+
 import httpService from "./lib/http/server";
 import app from "./lib/app";
 
-const port = 8000;
 
-app.attach(httpService);
 
-httpService.listen(port, () => {
-    console.log(`server is now listening on port ${port}`);
-})
+(function main()
+{
+    checkEnvironment();
+    startHttpServer();
+})();
+
+
+function startHttpServer()
+{
+    const port = parseInt(config.get("http.port"));
+
+    if (isNaN(port))
+    {
+        throw new Error(`"http.port" must be specified as an Integer`);
+    }
+
+    app.attach(httpService);
+
+    httpService.listen(port, () => {
+        console.log(`server is now listening on port ${port}`);
+    });
+}
+
+function checkEnvironment()
+{
+    const env = app.get("env");
+
+    if ( env === "development")
+    {
+        console.log(`Server is now started in ${env}`);
+
+    }
+    else 
+    {
+        throw new Error(`Unsupport deployment mode ${env}`);
+    }
+}
+
+
